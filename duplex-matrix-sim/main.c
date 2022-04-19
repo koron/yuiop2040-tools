1b7224dbef3e59b7eddbf4d2b262b2b4351f60b6
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-04-20 01:07:05 +0900

    arrange

--- duplex-matrix-sim/main.c
+++ duplex-matrix-sim/main.c
@@ -9,16 +9,7 @@
 #define SPI_TX      PICO_DEFAULT_SPI_TX_PIN
 #define SPI_CSN     PICO_DEFAULT_SPI_CSN_PIN
 
-int main() {
-    setup_default_uart();
-    stdio_init_all();
-    printf("\nDuplex Matrix Sim\n");
-
-    gpio_init(12);
-    gpio_init(13);
-    gpio_init(14);
-    gpio_init(15);
-
+void main1() {
     gpio_set_dir(12, true);
     //gpio_set_pulls(12, true, false);
     gpio_put(12, true);
@@ -42,4 +33,42 @@ int main() {
                 gpio_get(15) ? "Hi" : "Lo");
         sleep_ms(1000);
     }
+}
+
+void set_input_low(uint n) {
+    gpio_set_dir(n, false);
+    gpio_set_pulls(n, false, true);
+}
+
+void set_output_high(uint n) {
+    gpio_set_dir(n, true);
+    gpio_put(n, true);
+}
+
+void main2() {
+    set_input_low(12);
+    set_input_low(13);
+    set_input_low(14);
+    set_input_low(15);
+
+    set_output_high(15);
+
+    uint32_t n = 0;
+    while (true) {
+        printf("%03d #13=%s\n", n++, gpio_get(13) ? "Hi" : "Lo");
+        sleep_ms(1000);
+    }
+}
+
+int main() {
+    setup_default_uart();
+    stdio_init_all();
+    printf("\nDuplex Matrix Sim\n");
+
+    gpio_init(12);
+    gpio_init(13);
+    gpio_init(14);
+    gpio_init(15);
+
+    main2();
 }
