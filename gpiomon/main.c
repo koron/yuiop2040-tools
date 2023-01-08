a376090c01ea31c4d68665dcd9524cee07643ef1
MURAOKA Taro <koron.kaoriya@gmail.com>
2023-01-08 11:21:58 +0900

    declare UART pins explicitly. disable UART RX

--- gpiomon/main.c
+++ gpiomon/main.c
@@ -7,7 +7,7 @@ int main() {
     setup_default_uart();
     printf("\nYUIOP2040: GPIO monitor\n");
 
-    for (int i = 2; i < 30; i++) {
+    for (int i = 1; i < 30; i++) {
         gpio_init(i);
         gpio_set_dir(i, GPIO_IN);
         gpio_pull_up(i);
@@ -17,7 +17,7 @@ int main() {
     uint32_t last_gpio = 0;
     while(true) {
         uint64_t now = time_us_64();
-        uint32_t curr = gpio_get_all() & 0x3ffffffc;
+        uint32_t curr = gpio_get_all() & 0x3ffffffe;
         if (last_gpio != curr && now - last_change >= 5000) {
             printf("GPIO: %08lx at %llu\n", curr, now);
             last_gpio = curr;
